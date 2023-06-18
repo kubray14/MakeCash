65db6808edd8b08f9033876d984297065a31f295
Huseyin <akcali-1997@hotmail.com>
2023-06-18 13:54:40 +0300

    Added fan speed increase with pipe speed

--- Assets/Scripts/FanSpin.cs
+++ Assets/Scripts/FanSpin.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,22 @@ using UnityEngine;
 public class FanSpin : MonoBehaviour
 {
 
-    [SerializeField] private float rotationSpeed = 50f;
-    [SerializeField] private float minRotationSpeed = 50f;
-    [SerializeField] private float maxRotationSpeed = 150f;
+    [SerializeField] private static float rotationSpeed = 50f;
+    [SerializeField] private static float minRotationSpeed = 50f;
+    [SerializeField] private static float maxRotationSpeed = 150f;
+    private static float speedMultiplier = 1f;
     private Vector3 turnDirection = -Vector3.forward;
 
     private void Start()
     {
         EventManager.onSpinChange.AddListener(spinChange);
+        EventManager.OnSpeedUpgrade.AddListener(IncreaseSpeedMultiplier);
+        speedMultiplier = 1f;
     }
 
     private void Update()
     {
-        transform.Rotate(turnDirection, rotationSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(turnDirection, rotationSpeed * speedMultiplier * Time.deltaTime, Space.World);
     }
 
     private void spinChange(bool stat)
@@ -30,5 +34,10 @@ public class FanSpin : MonoBehaviour
         {
             rotationSpeed = minRotationSpeed;
         }
+    }
+
+    private void IncreaseSpeedMultiplier()
+    {
+        speedMultiplier += 0.05f;
     }
 }
